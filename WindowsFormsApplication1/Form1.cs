9d7bf7981dc921073a735d51c36d72e8cce53f68
taka <takahiro.shigemoto@gmail.com>
2016-09-03 17:45:04 +0900

    プロジェクト ファイルを追加します。

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using SuperSocket.ClientEngine;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Threading;
+using System.Net.WebSockets;
+
+// see http://kimux.net/?p=929
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Send message buffer size.
+        /// </summary>
+        const int MessageBufferSize = 256;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Connect();
+        }
+
+        private async void Connect()
+        {
+            ClientWebSocket _ws = new ClientWebSocket();
+
+            if (_ws.State != WebSocketState.Open)
+            {
+                await _ws.ConnectAsync(new Uri("ws://localhost:8080/echo"), CancellationToken.None);
+
+                while (_ws.State == WebSocketState.Open)
+                {
+                    var buff = new ArraySegment<byte>(new byte[MessageBufferSize]);
+                    var ret = await _ws.ReceiveAsync(buff, CancellationToken.None);
+                    listBox1.Items.Add((new UTF8Encoding()).GetString(buff.Take(ret.Count).ToArray()));
+                }
+            }
+        }
+    }
+}
